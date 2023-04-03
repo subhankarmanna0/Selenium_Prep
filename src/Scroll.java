e2830cadf62dab0b87c1346dfef192e61800f310
subhankarmanna0 <subhankarm407@gmail.com>
2023-04-03 20:56:14 +0530

    How to scroll web page

--- src/Scroll.java
+++ src/Scroll.java
@@ -1,6 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+
+
 
 public class Scroll 
 {
@@ -18,6 +25,24 @@ public class Scroll
 		js.executeScript("window.scrollBy(0, 500)");
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		
+		List<WebElement>values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		int sum = 0;
+		for(int i = 0; i < values.size(); i++)
+		{
+			int number = Integer.parseInt(values.get(i).getText());
+			System.out.println(number);
+			sum = number +sum;
+		}
+		System.out.println("Sum of table values: "+sum);
+		String total = driver.findElement(By.cssSelector(".totalAmount")).getText();
+		
+		//Integer.parseInt(total);
+		String[] TotalNum = total.split(":");
+		System.out.println("Sum from screen: "+TotalNum[1].trim());
+		int screenNum = Integer.parseInt(TotalNum[1].trim());
+		
+		Assert.assertEquals(sum, screenNum);
 
 	}
 
